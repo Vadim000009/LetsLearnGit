8d60e2b793bee6eac271e312f6e38d07fe216943
Vadim000009 <tuukvadim@live.com>
2020-10-23 12:32:50 +0300

    Корректировка мейна

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-from funcRodionov import getArrWords
+# import alphabet
+# import abcExample
+# import abcBySmolnaya
+# import Alphabet_Overchuk
+# import alphabet
+# import alphabetClass
+import abcByRVS
 
 if __name__ == "__main__":
-    print("Fork")
-    # a, b = input(), input()
-    a, b = "aab", "abb"
-    getArrWords(a, b)
+    print("hello world")
+    start = input("Enter a start string: ")  # aaa
+    finish = input("Enter where to finish: ")  # aax
+    # abcExample.abc(start, finish)
+    # abcBySmolnaya.abc(start, finish)
+    # alphabet.abc(start, finish)
+    # abcExample.abc(start, finish)
+    # Alphabet_Overchuk.Alphabet(start, finish)
+    # alphabet.alphabetV2(start, finish)
+    abcByRVS.alphabet(start, finish)
+    # try:
+    #     alphabetClass.Alphabet('abcdefghijklmnopqrstuvwxyz').print_range(start, finish)
+    # except ValueError as errors:
+    #     print(errors)
+    #     exit(1)
+    # except KeyError:
+    #     print('Input string have unknown symbol')
+    #     exit(2)
